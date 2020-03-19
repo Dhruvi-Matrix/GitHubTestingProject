c198e867783e78d0ec40a733908040c615274568
dhruvi.vaza <dhruvi.vaza@matrixhive.com>
2020-03-19 16:01:05 +0530

    third

--- app/src/main/java/com/example/githubtestingproject/MainActivity.kt
+++ app/src/main/java/com/example/githubtestingproject/MainActivity.kt
@@ -10,6 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textView.text=getString(R.string.app_name)
+        textView.text=getString(R.string.github_msg)
     }
 }
